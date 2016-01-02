f1f5a95ec61d73d1588a58bef530abf05cca6a07
Antony74 <akb@akb.me.uk>
2016-01-02 21:58:34 +0000

    Successfully collapsed the stack so the whole thing runs in-browser for demo purposes.

--- DevUtils/browserify.js
+++ DevUtils/browserify.js
@@ -9,15 +9,18 @@ if (!fs.existsSync('../browserified'))
 // Tier 1 just needs an extra <script> tag
 
 var arrLines = fs.readFileSync(__dirname + '/../source/Tier1-UI.html', {encoding: 'utf8'}).split('<script');
-arrLines[0] += '<script src="Tier2-Middle.js"></script>\r\n    ';
+arrLines[2] += '<script src="Tier2-Middle.js"></script>\r\n    ';
 fs.writeFile(__dirname + '/../browserified/Tier1-UI.html', arrLines.join('<script'), {encoding: 'utf8'});
 
 // Tier 2 needs browserifying
-
+/*
 var browserify = require('browserify');
 var b = browserify();
 b.add(__dirname +'/../source/Tier2-Middle.js');
 b.bundle().pipe(fs.createWriteStream(__dirname + '/../browserified/Tier2-Middle.js'));
+*/
+
+fs.createReadStream(__dirname + '/../source/Tier2-Middle.js').pipe(fs.createWriteStream(__dirname + '/../browserified/Tier2-Middle.js'));
 
 // Tier 3 just needs copying
 
--- source/Tier2-Middle.js
+++ source/Tier2-Middle.js
@@ -1,112 +1,228 @@
 
-var bodyparser = require('body-parser');
-var express = require('express');
-var status = require('http-status');
-var fs = require('fs');
+var db = {};
 
-var db = JSON.parse(fs.readFileSync(__dirname + '/Tier3-DB.json'));
-var arrIndex = createIndex(db);
-var nextClientID = arrIndex.length + 1;
+var callbackQueue = [];
 
-var app = express();
-
-app.use('/api/v1', getApi());
-
-app.get('/', function(req, res)
+/*global window*/
+if (typeof(window) !== 'undefined')
 {
-    res.sendFile(__dirname + '/Tier1-UI.html');
-});
+    window.threeTieredExampleApi =
+    {
+        read: function($http, clientId, callback)
+        {
+            callbackQueue.push(['read', [$http, clientId, callback]]);
+        },
 
-var port = 8080;
-var sServerUrl = 'http://localhost:' + port + '/';
+        readPage: function($http, pageNumber, callback)
+        {
+            callbackQueue.push(['readPage', [$http, pageNumber, callback]]);
+        },
 
-app.listen(port);
-console.log('Server has been started (' + sServerUrl + ')');
+        update: function($http, clientId, client, callback)
+        {
+            callbackQueue.push(['update', [$http, clientId, client, callback]]);
+        },
+
+        delete: function($http, clientId, callback)
+        {
+            callbackQueue.push(['delete', [$http, clientId, callback]]);
+        }
+    };
+
+    /*global angular*/
+    angular.injector(['ng']).invoke(function($http)
+    {
+        $http.get('Tier3-DB.json').success(exposeDatabase);
+    });
+}
+else
+{
+    var fs = require('fs');
+    var db = JSON.parse(fs.readFileSync(__dirname + '/Tier3-DB.json'));
+    exposeDatabase(db);
+}
 
 //
-// getApi
+// exposeDatabase
 //
-function getApi()
+function exposeDatabase(db)
 {
-    var api = express.Router();
-    api.use(bodyparser.json());
+    var arrIndex = createIndex(db);
+    var nextClientID = arrIndex.length + 1;
+
+    var api =
+    {
+        // Create
+        create: function($http, client, callback)
+        {
+            db[nextClientID.toString()] = client;
+            var retval = {id: nextClientID};
+            ++nextClientID;
+            arrIndex = createIndex(db);
+            callback(retval);
+        },
+
+        // Read
+        read: function($http, clientId, callback)
+        {
+            var client = db[clientId];
+            callback({data: client});
+        },
+
+        readPage: function($http, nPageNum, callback)
+        {
+            nPageNum = parseInt(nPageNum);
+
+            var PAGESIZE = 20;
+            var nStart = (nPageNum-1) * 20;
+            var arrItems = [];
+
+            for (var n = 0; n < PAGESIZE; ++n)
+            {
+                var nIndex = nStart + n;
+
+                if (nIndex >=0 && nIndex < arrIndex.length)
+                {
+                    var client = {clientId: arrIndex[nIndex], fields: db[arrIndex[nIndex]]};
+                    arrItems.push(client);
+                }
+            }
+
+            var arrLinks = [];
+
+            if (nPageNum > 1)
+            {
+                arrLinks.push({'rel': 'First', 'pageNumber': 1});
+                arrLinks.push({'rel': 'Previous', 'pageNumber': (nPageNum - 1)});
+            }
+            else
+            {
+                arrLinks.push({'rel': 'First'});
+                arrLinks.push({'rel': 'Previous'});
+            }
+
+            if (PAGESIZE * nPageNum < arrIndex.length)
+            {
+                arrLinks.push({'rel': 'Next', 'pageNumber': (nPageNum + 1)});
+                arrLinks.push({'rel': 'Last', 'pageNumber': Math.floor(arrIndex.length/PAGESIZE)});
+            }
+            else
+            {
+                arrLinks.push({'rel': 'Next'});
+                arrLinks.push({'rel': 'Last'});
+            }
+
+            callback(
+            {
+                'data': arrItems,
+                'links': arrLinks
+            });
+        },
+
+        // Update
+        update: function($http, clientId, client, callback)
+        {
+            db[clientId] = client;
+            callback();
+        },
+
+        // Delete
+        delete: function($http, clientId, callback)
+        {
+            if (db[clientId] === undefined)
+            {
+                callback(false);
+            }
+            else
+            {
+                delete db[clientId];
+                arrIndex = createIndex(db);
+                callback(true);
+            }
+        },
+    };
+
+    if (typeof(window) !== 'undefined')
+    {
+        // We are in the browser so expose the JavaScript API
+        window.threeTieredExampleApi = api;
+
+        // Also make any API calls which have been queued
+        while (callbackQueue.length)
+        {
+            var queuedItem = callbackQueue.shift();
+            api[queuedItem[0]].apply(api, queuedItem[1]);
+        }
+    }
+    else
+    {
+        // We are not in the browser so expose the http API
+        var express = require('express');
+
+        var app = express();
+
+        app.use('/api/v1', getHttpApi(api));
+
+        app.get('/', function(req, res)
+        {
+            res.sendFile(__dirname + '/Tier1-UI.html');
+        });
+
+        var port = 8080;
+        var sServerUrl = 'http://localhost:' + port + '/';
+
+        app.listen(port);
+        console.log('Server has been started (' + sServerUrl + ')');
+    }
+}
+
+//
+// getHttpApi
+//
+function getHttpApi(api)
+{
+    var bodyparser = require('body-parser');
+    var express = require('express');
+    var status = require('http-status');
+
+    var httpApi = express.Router();
+    httpApi.use(bodyparser.json());
 
     // Create
-    api.post('/clients', function(req, res)
+    httpApi.post('/clients', function(req, res)
     {
-        db[nextClientID.toString()] = req.body;
-        res.json({id: nextClientID});
-        ++nextClientID;
-        arrIndex = createIndex(db);
+        api.create(null, req.body, function(data)
+        {
+            res.json(data);
+        });
     });
 
     // Read
-    api.get('/clients/:id', function(req, res)
+    httpApi.get('/clients/:id', function(req, res)
     {
-        var client = db[req.params.id];
-
-        if (client === undefined)
+        api.read(null, req.params.id, function(data)
         {
-            res.status(status.NOT_FOUND).json({ error: 'Not found' });
-        }
-        else
-        {
-            res.json({data: client});
-        }
+            if (data.data === undefined)
+            {
+                res.status(status.NOT_FOUND).json({ error: 'Not found' });
+            }
+            else
+            {
+                res.json(data);
+            }
+        });
     });
 
-    api.get('/clients/pages/:pagenum', function(req, res)
+    httpApi.get('/clients/pages/:pagenum', function(req, res)
     {
-        var PAGESIZE = 20;
-        var nPageNum = parseInt(req.params.pagenum);
-        var nStart = (nPageNum-1) * 20;
-        var arrItems = [];
-
-        for (var n = 0; n < PAGESIZE; ++n)
+        api.readPage(null, req.params.pagenum, function(data)
         {
-            var nIndex = nStart + n;
-
-            if (nIndex >=0 && nIndex < arrIndex.length)
-            {
-                var client = {clientId: arrIndex[nIndex], fields: db[arrIndex[nIndex]]};
-                arrItems.push(client);
-            }
-        }
-
-        var arrLinks = [];
-
-        if (req.params.pagenum > 1)
-        {
-            arrLinks.push({'rel': 'First', 'pageNumber': 1});
-            arrLinks.push({'rel': 'Previous', 'pageNumber': (nPageNum - 1)});
-        }
-        else
-        {
-            arrLinks.push({'rel': 'First'});
-            arrLinks.push({'rel': 'Previous'});
-        }
-
-        if (PAGESIZE * req.params.pagenum < arrIndex.length)
-        {
-            arrLinks.push({'rel': 'Next', 'pageNumber': (nPageNum + 1)});
-            arrLinks.push({'rel': 'Last', 'pageNumber': Math.floor(arrIndex.length/PAGESIZE)});
-        }
-        else
-        {
-            arrLinks.push({'rel': 'Next'});
-            arrLinks.push({'rel': 'Last'});
-        }
-
-        res.json(
-        {
-            'data': arrItems,
-            'links': arrLinks
+            res.json(data);
         });
-
     });
 
     // Update
-    api.put('/clients/:id', function(req, res)
+    httpApi.put('/clients/:id', function(req, res)
     {
         if (db[req.params.id] === undefined)
         {
@@ -120,21 +236,22 @@ function getApi()
     });
 
     // Delete
-    api.delete('/clients/:id', function(req, res)
+    httpApi.delete('/clients/:id', function(req, res)
     {
-        if (db[req.params.id] === undefined)
+        api.delete(null, req.params.id, function(bDone)
         {
-            res.status(status.NOT_FOUND).json({ error: 'Not found' });
-        }
-        else
-        {
-            delete db[req.params.id];
-            arrIndex = createIndex(db);
-            res.send();
-        }
+            if (bDone)
+            { 
+                res.send();
+            }
+            else
+            {
+                res.status(status.NOT_FOUND).json({ error: 'Not found' });
+            }
+        });
     });
 
-    return api;
+    return httpApi;
 }
 
 //
