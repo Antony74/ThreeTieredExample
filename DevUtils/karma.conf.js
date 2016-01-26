9c4375616b2a17b48aba83bd488cc0366f316ee3
Antony <akb@akb.me.uk>
2016-01-26 16:43:10 +0000

    Finish off configuration for controller tests.

---
+++ DevUtils/karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '../karmaified',
+    frameworks: ['mocha'],
+    files: [
+		'angular.js',
+		'angular-route.js',
+		'angular-mocks.js',
+		'app.js',
+		'../devutils/testControllers.js'
+    ],
+    port: 8080,
+    browsers: ['Chrome'],
+    singleRun: true
+  });
+};
+
---
+++ DevUtils/testControllers.js
@@ -0,0 +1,10 @@
+/*jslint mocha: true */
+
+describe('Clients controller', function()
+{
+	it('does nothing yet', function(done)
+	{
+		done();
+	});
+});
+
